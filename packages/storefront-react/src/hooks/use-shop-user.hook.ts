be148cd4ddaf41d4980b7423467c0b034fa20840
Jonas Embach <je@marmalade.group>
2022-07-06 11:23:33 +0200

    feat(useShopCart): add fields productsInCart and quantityInCart
    - docs: add typescript comments to all hooks for better DX accessing returned variables

--- packages/storefront-react/src/hooks/use-shop-user.hook.ts
+++ packages/storefront-react/src/hooks/use-shop-user.hook.ts
@@ -1,7 +1,15 @@
 import { useContext } from 'react'
-import { ShopContext } from '../context'
+import { ShopContext, ShopContextData } from '../context'
 
-export function useShopUser() {
+export type UseShopUserData = {
+  /**
+   * The current user. Is undefined when no user is
+   * logged in or when reloading the user fails.
+   */
+  user: ShopContextData['user']
+}
+
+export function useShopUser(): UseShopUserData {
   const { user } = useContext(ShopContext)
 
   return { user }
--- packages/storefront-react/src/hooks/use-shop-wishlist.hook.ts
+++ packages/storefront-react/src/hooks/use-shop-wishlist.hook.ts
@@ -1,7 +1,14 @@
 import { useContext } from 'react'
-import { ShopContext } from '../context'
+import { ShopContext, ShopContextData } from '../context'
 
-export function useShopWishlist() {
+export type UseShopWishlistData = {
+  /**
+   * The current wishlist with the products
+   */
+  wishlist: ShopContextData['wishlist']
+}
+
+export function useShopWishlist(): UseShopWishlistData {
   const { wishlist } = useContext(ShopContext)
 
   return { wishlist }
