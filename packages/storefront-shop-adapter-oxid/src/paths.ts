55ae9ee025320fd980d4c3347a5a9b6f1e3659ff
Jonas Embach <je@marmalade.group>
2022-07-19 15:19:18 +0200

    fix(oxid): set correct url paths

--- packages/storefront-shop-adapter-oxid/src/paths.ts
+++ packages/storefront-shop-adapter-oxid/src/paths.ts
@@ -3,13 +3,13 @@ export const USER_GET_CURRENT =
   '/index.php?cl=MakairaUserController&fnc=getCurrentLoggedInUser'
 export const USER_LOGOUT = '/index.php?cl=MakairaUserController&fnc=logout'
 
-export const CART_GET = '/index.php?cl=MakairaUserController&fnc=getCartItems'
+export const CART_GET = '/index.php?cl=MakairaCartController&fnc=getCartItems'
 export const CART_ADD =
-  '/index.php?cl=MakairaUserController&fnc=addProductToCart'
+  '/index.php?cl=MakairaCartController&fnc=addProductToCart'
 export const CART_REMOVE =
-  '/index.php?cl=MakairaUserController&fnc=removeCartItem'
+  '/index.php?cl=MakairaCartController&fnc=removeCartItem'
 export const CART_UPDATE =
-  '/index.php?cl=MakairaUserController&fnc=updateCartItem'
+  '/index.php?cl=MakairaCartController&fnc=updateCartItem'
 
 export const REVIEW_GET = '/index.php?cl=MakairaReviewController&fnc=getReviews'
 export const REVIEW_CREATE =
