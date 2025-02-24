3f2555e80332c4de6d39c7b99a51a52e35acaa99
phongnguyenhh1996 <34834380+phongnguyenhh1996@users.noreply.github.com>
2025-02-24 14:17:53 +0700

    fix(GHAction): Update renovate.config.js

--- renovate.config.js
+++ renovate.config.js
@@ -2,13 +2,4 @@ module.exports = {
   repositories: ['MakairaIO/storefront-shop-adapter'],
   branchPrefix: 'renovate/',
   gitAuthor: process.env.GIT_AUTHOR,
-  noAuth: false,
-  hostRules: [
-    {
-      matchHost: 'ghcr.io',
-      username: 'token',
-      password: process.env.GITHUB_TOKEN,
-      noAuth: false,
-    },
-  ],
 }
